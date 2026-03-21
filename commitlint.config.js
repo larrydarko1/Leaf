18f9910f58a0c62a8d4501f0a2307a805c3811db
Larry Darko <l.mazzola96@tutanota.com>
2026-03-21 12:59:17 +0100

    chore: added more commitlint rules for body character size with more than 100 characters

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'header-max-length': [2, 'always', 200],
+        'body-max-line-length': [2, 'always', 200],
         'type-enum': [
             2,
             'always',
