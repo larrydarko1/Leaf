9e222ff9c8a479a151ad6a5abd4d2df5c7a54b77
Larry <l.mazzola96@tutanota.com>
2026-05-15 12:19:57 +0200

    fix: regex issue that was crashing input

--- src/renderer/components/SearchPanel.vue
+++ src/renderer/components/SearchPanel.vue
@@ -121,8 +121,8 @@ function openSelectedResult() {
 function highlightMatch(text: string): string {
     if (!searchQuery.value.trim()) return text;
 
-    const query = searchQuery.value;
-    const regex = new RegExp(`(${query})`, 'gi');
+    const escaped = searchQuery.value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const regex = new RegExp(`(${escaped})`, 'gi');
     return text.replace(regex, '<mark>$1</mark>');
 }
 </script>
