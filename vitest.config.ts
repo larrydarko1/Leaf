59d7a1c9d467dcb7aeb23609f57c0c68155a3561
Larry <l.mazzola96@tutanota.com>
2026-06-19 18:21:44 +0200

    test: forgot to resolve the new alias and it broke some tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import { configDefaults } from 'vitest/config';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@/main': fileURLToPath(new URL('./src/main', import.meta.url)),
+            '@/renderer': fileURLToPath(new URL('./src/renderer', import.meta.url)),
+            '@/schemas': fileURLToPath(new URL('./src/schemas', import.meta.url)),
+            '@/preload': fileURLToPath(new URL('./src/preload', import.meta.url)),
+        },
+    },
     esbuild: { tsconfigRaw: { compilerOptions: { target: 'ESNext' } } },
     test: {
         globals: true,
